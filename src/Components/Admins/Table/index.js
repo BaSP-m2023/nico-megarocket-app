4c4fca3fe0437feaa2110161c8c668a68700c3d9
Mauro Jimenez <maurojim123@gmail.com>
2023-05-27 14:04:28 -0300

    MR-65: changes in table

--- src/Components/Admins/Table/index.js
+++ src/Components/Admins/Table/index.js
@@ -1,7 +1,12 @@
-import React from 'react';
+// import React, { useState } from 'react';
 import styles from './table.module.css';
 
 function index({ admins }) {
+  const deleteion = () => {
+    console.log('se borra pero no se como xd');
+    console.log(admins);
+  };
+
   return (
     <div className={styles.container}>
       <table className={styles.table}>
@@ -27,12 +32,32 @@ function index({ admins }) {
                 <td>{item.phone}</td>
                 <td>{item.email}</td>
                 <td>{item.city}</td>
-                <td>!</td>
-                <td>X</td>
+                <td>
+                  <img
+                    className={styles.trash_edit}
+                    src={`${process.env.PUBLIC_URL}/assets/images/pencil-edit.svg`}
+                    alt="modify icon"
+                  />
+                </td>
+                <td>
+                  <img
+                    onClick={deleteion}
+                    className={styles.trash_edit}
+                    src={`${process.env.PUBLIC_URL}/assets/images/trash-delete.svg`}
+                    alt="delete icon"
+                  />
+                </td>
               </tr>
             );
           })}
         </tbody>
+        <tfoot>
+          <tr>
+            <td colSpan={7} className={styles.add_admin}>
+              Create new admin
+            </td>
+          </tr>
+        </tfoot>
       </table>
     </div>
   );
