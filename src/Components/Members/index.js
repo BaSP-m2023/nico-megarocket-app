f352814fd3dddb4cd38b835a544944f847bbd1d0
FrancoLelli <francogastonlelli@gmail.com>
2023-05-27 16:52:44 -0300

    MR-75: Create function to receive members from the server

--- src/Components/Members/index.js
+++ src/Components/Members/index.js
@@ -3,23 +3,24 @@ import styles from './members.module.css';
 
 function Members() {
   const [members, setMembers] = useState([]);
+  const getMembers = async () => {
+    try {
+      const reponse = await fetch(`${process.env.REACT_APP_API_URL}/member`);
+      const data = await reponse.json();
+      setMembers(data.data);
+      console.log(members);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   useEffect(() => {
-    fetch(`https://jsonplaceholder.typicode.com/users`)
-      .then((response) => response.json())
-      .then((response) => {
-        setMembers(response);
-      });
+    getMembers();
   }, []);
 
   return (
     <section className={styles.container}>
       <h2>Members</h2>
-      <div>
-        {members.map((employee) => {
-          return <div key={employee.id}>{employee.name}</div>;
-        })}
-      </div>
     </section>
   );
 }
