78e5b6672fbc19c2beca0dc8230cbd8ce936e827
FrancoLelli <francogastonlelli@gmail.com>
2023-05-27 17:33:46 -0300

    MR-75: Add styles in table

--- src/Components/TableMember/index.js
+++ src/Components/TableMember/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import styles from './table-member.module.css';
 
 const TableMember = ({ members }) => {
   console.log(members);
   return (
-    <div>
-      <table>
-        <tr>
+    <div className={styles.container}>
+      <table className={styles.table}>
+        <tr className={styles.tableContent}>
           <th>Full Name</th>
           <th>Email</th>
           <th>Phone</th>
@@ -16,6 +17,22 @@ const TableMember = ({ members }) => {
           <th>Edit</th>
           <th>Delete</th>
         </tr>
+        {members.map((member, index) => {
+          const rowClass = index % 2 === 0 ? styles.rowBackground1 : styles.rowBackground2;
+          return (
+            <tr className={rowClass} key={index}>
+              <td>
+                {member.firstName} {member.lastName}
+              </td>
+              <td>{member.email}</td>
+              <td>{member.phone}</td>
+              <td>{member.city}</td>
+              <td>{member.postalCode}</td>
+              <td>{member.isActive ? 'Active' : 'Inactive'}</td>
+              <td>{member.membership}</td>
+            </tr>
+          );
+        })}
       </table>
     </div>
   );
