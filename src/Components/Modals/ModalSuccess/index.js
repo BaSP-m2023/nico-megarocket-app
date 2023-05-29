cb47529f55ba5b560513f0a86c03d72b662e7380
FrancoLelli <francogastonlelli@gmail.com>
2023-05-29 14:31:50 -0300

    MR-82: Add styles to Success Modal

--- src/Components/Modals/ModalSuccess/index.js
+++ src/Components/Modals/ModalSuccess/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import styles from './modalSuccess.module.css';
 
 const ModalSuccess = ({ setModalSuccessOpen, message }) => {
   return (
-    <div>
-      <div>
-        <p>SUCCESSFUL</p>
-        <p>{message}</p>
-        <button onClick={() => setModalSuccessOpen(false)}>OK</button>
+    <div className={styles.modalAlert}>
+      <div className={styles.modalStyles}>
+        <p className={styles.titleModal}>SUCCESSFUL</p>
+        <p className={styles.textModal}>{message}</p>
+        <button className={styles.btnAccept} onClick={() => setModalSuccessOpen(false)}>
+          OK
+        </button>
       </div>
     </div>
   );
