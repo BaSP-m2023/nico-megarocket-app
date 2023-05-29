67d1cab114156dacf254fbf5d48eec0692704269
FrancoLelli <francogastonlelli@gmail.com>
2023-05-29 20:18:42 -0300

    MR-82: Fix code

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,11 +1,9 @@
-import ModalError from '../Modals/ModalError';
 import styles from './home.module.css';
 
 function Home() {
   return (
     <section className={styles.container}>
       <h2>Home</h2>
-      <ModalError />
     </section>
   );
 }
