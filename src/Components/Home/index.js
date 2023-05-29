b73027420e05e9fa322864ad1adc301a16d4c0cc
FrancoLelli <francogastonlelli@gmail.com>
2023-05-29 20:18:01 -0300

    MR-82: Update styles in Modal Error

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,9 +1,11 @@
+import ModalError from '../Modals/ModalError';
 import styles from './home.module.css';
 
 function Home() {
   return (
     <section className={styles.container}>
       <h2>Home</h2>
+      <ModalError />
     </section>
   );
 }
--- src/Components/Modals/ModalError/index.js
+++ src/Components/Modals/ModalError/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import styles from './ModalError.module.css';
+import styles from './modalError.module.css';
 
 const ModalError = ({ setModalErroOpen, message }) => {
   return (
     <div className={styles.modalAlert}>
-      <div className={styles.modalStyles}>
+      <div className={styles.headerModal}>
         <p className={styles.titleModal}>ERROR</p>
-        <p className={styles.textModal}>{message}</p>
-        <button className={styles.btnAccept} onClick={() => setModalErroOpen(false)}>
-          OK
+        <button className={styles.btnClose} onClick={() => setModalErroOpen(false)}>
+          X
         </button>
       </div>
+      <p className={styles.textModal}>{message}</p>
     </div>
   );
 };
