3fc23a923710f1e74dcfcc9f568b7ebaf62a89ab
Mateo Carciente <mateocarciente1@gmail.com>
2023-05-30 18:48:57 -0300

    MR-73:added modals

--- src/Components/Classes/Table/index.jsx
+++ src/Components/Classes/Table/index.jsx
@@ -1,46 +1,84 @@
+import React, { useState } from 'react';
 import styles from './table.module.css';
-import React, { useEffect, useState } from 'react';
-import Table from './Table';
+import ModalsConfirmation from '../../Modals/ModalConfirm';
 
-function Projects() {
-  const [classes, setClasses] = useState([]);
-  const getClasses = async () => {
-    try {
-      const response = await fetch(`${process.env.REACT_APP_API_URL}/class`);
-      const data = await response.json();
-      setClasses(data);
-      console.log(response);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  useEffect(() => {
-    getClasses();
-  }, []);
+const Table = ({ data, deleteClass, editClass }) => {
+  const [modalDeleteConfirmOpen, setModalDeleteConfirmOpen] = useState(false);
+  const [selectedItemId, setSelectedItemId] = useState('');
 
-  const deleteClassfromDB = async (id) => {
-    try {
-      await fetch(`${process.env.REACT_APP_API_URL}/class/${id}`, {
-        method: 'DELETE',
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      });
-    } catch (error) {
-      console.error(error);
-    }
+  const handleDeleteButtonClick = (id) => {
+    setSelectedItemId(id);
+    setModalDeleteConfirmOpen(true);
   };
 
-  const deleteClass = async (id) => {
-    await deleteClassfromDB(id);
-    setClasses({ ...classes.filter((classes) => classes.id !== id) });
+  const handleModalConfirmation = () => {
+    deleteClass(selectedItemId);
+    setModalDeleteConfirmOpen(false);
   };
 
   return (
     <section className={styles.container}>
-      <Table data={classes} deleteClass={deleteClass} />
+      <button className={styles.addClassButton}>Add Class</button>
+      <table>
+        <thead>
+          <tr>
+            <th>Activity</th>
+            <th>Hour</th>
+            <th>Trainer</th>
+            <th>Places</th>
+            <th>Edit</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        {!data ? (
+          <div className={styles.nonTrainer}>
+            <div>
+              <h3>The list is empty</h3>
+            </div>
+          </div>
+        ) : (
+          <tbody>
+            {data.map((item, index) => {
+              return (
+                <tr key={index}>
+                  <td>{item.activity ? item.activity.name : ''}</td>
+                  <td>{item.hour}</td>
+                  <td>
+                    {item.trainer.map((trainerOne) => {
+                      return `${trainerOne.firstName} ${trainerOne.lastName}`;
+                    })}
+                  </td>
+                  <td>{item.slots}</td>
+                  <td>
+                    <img
+                      className={styles.trash_edit}
+                      onClick={() => editClass(item._id)}
+                      src={`${process.env.PUBLIC_URL}/assets/images/pencil-edit.svg`}
+                    />
+                  </td>
+                  <td>
+                    <img
+                      className={styles.trash_edit}
+                      onClick={() => handleDeleteButtonClick(item._id)}
+                      src={`${process.env.PUBLIC_URL}/assets/images/trash-delete.svg`}
+                    />
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        )}
+        {modalDeleteConfirmOpen && (
+          <ModalsConfirmation
+            method="Delete"
+            onConfirm={handleModalConfirmation}
+            setModalConfirmOpen={setModalDeleteConfirmOpen}
+            message="Are you sure you want to delete this?"
+          />
+        )}
+      </table>
     </section>
   );
-}
+};
 
-export default Projects;
+export default Table;
--- src/Components/Classes/index.js
+++ src/Components/Classes/index.js
@@ -1,11 +1,54 @@
 import styles from './classes.module.css';
-import Table from '../Classes/Table';
+import React, { useEffect, useState } from 'react';
+import Table from './Table';
+import ModalsSuccess from '../Modals/ModalSuccess';
 
 function Projects() {
+  const [classes, setClasses] = useState([]);
+  const [modalSuccessOpen, setModalSuccessOpen] = useState(false);
+  const [successMessage, setSuccessMessage] = useState('');
+  const getClasses = async () => {
+    try {
+      const response = await fetch(`http://localhost:4000/api/class`);
+      const data = await response.json();
+      setClasses(data.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    getClasses();
+  }, []);
+  console.log(classes);
+
+  const deleteClassfromDB = async (id) => {
+    try {
+      await fetch(`http://localhost:4000/api/class/${id}`, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+      setClasses([...classes.filter((classes) => classes._id !== id)]);
+      setSuccessMessage('The class has been deleted successfully.');
+      setModalSuccessOpen(true);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const deleteClass = (id) => {
+    deleteClassfromDB(id);
+  };
+
   return (
     <section className={styles.container}>
-      <h2>Classes</h2>
-      <Table />
+      <div>
+        {modalSuccessOpen && (
+          <ModalsSuccess setModalSuccessOpen={setModalSuccessOpen} message={successMessage} />
+        )}
+      </div>
+      <Table data={classes} deleteClass={deleteClass} />
     </section>
   );
 }
