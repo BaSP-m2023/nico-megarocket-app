6141af1b1ea39d8a38c0ce158613b31f5f43c8d2
Mauro Jimenez <maurojim123@gmail.com>
2023-06-03 14:00:12 -0300

    MR-87: Styles for ModalConfirm

--- src/Components/Modals/ModalConfirm/index.js
+++ src/Components/Modals/ModalConfirm/index.js
@@ -5,19 +5,39 @@ const ModalConfirm = ({ message, method, onConfirm, setModalConfirmOpen }) => {
   return (
     <div className={styles.modalAlert}>
       <div className={styles.modalStyles}>
-        <p className={styles.titleModal}>Attention</p>
-        <p className={styles.textModal}>{message}</p>
-        <button className={styles.btnAccept} onClick={onConfirm}>
-          {method}
-        </button>
-        <button
-          className={styles.btnCancel}
+        <img
+          className={styles.close_icon}
           onClick={() => {
             setModalConfirmOpen(false);
           }}
+          src={`${process.env.PUBLIC_URL}/assets/images/Delete.svg`}
+          alt="cancel icon"
+        />
+        <p
+          className={styles.titleModal}
+          style={method.toLowerCase() === 'delete' ? { color: '#F13312' } : { color: '#94ca7d' }}
         >
-          Cancel
-        </button>
+          Attention
+        </p>
+        <p className={styles.textModal}>{message}</p>
+        <div className={styles.buttons}>
+          <button
+            className={styles.btnCancel}
+            onClick={() => {
+              setModalConfirmOpen(false);
+            }}
+          >
+            Cancel
+          </button>
+          <button
+            className={
+              method.toLowerCase() === 'delete' ? styles.btnAcceptDelete : styles.btnAccept
+            }
+            onClick={onConfirm}
+          >
+            {method}
+          </button>
+        </div>
       </div>
     </div>
   );
