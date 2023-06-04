069591d696f486e8ada52d392d466152c6dbe7d6
Mateo Carciente <mateocarciente1@gmail.com>
2023-06-04 13:00:21 -0300

    MR-86: change routes and add details

--- src/Components/Shared/index.js
+++ src/Components/Shared/index.js
@@ -1,5 +1,5 @@
 export { default as ModalConfirm } from './Modals/ModalConfirm';
 export { default as ModalSuccess } from './Modals/ModalSuccess';
 export { default as ToastError } from './Modals/ToastError';
-export { default as Inputs } from './DisableInput';
-export { default as OptionInput } from './OptionInput';
+export { default as Inputs } from './Inputs/NormalInput';
+export { default as OptionInput } from './Inputs/OptionInput';
