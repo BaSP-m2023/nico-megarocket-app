536b06113f06a8f172e86f28d61ad4bfd5044525
Emanuel-Lamberti <elemanu9222@gmail.com>
2023-06-04 16:31:22 -0300

    MR-86: inputs error and placeholder

---
+++ src/Components/Shared/Inputs/InputError/index.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import './error-placeholder-input.css';
+
+const ErrorInput = ({ value, type, error, change, placeholder }) => {
+  const [focus, setFocus] = useState(false);
+
+  const handleFocus = () => {
+    setFocus(true);
+  };
+
+  const handleBlur = () => {
+    setFocus(false);
+  };
+
+  const inputClassName = `input ${error && !focus ? 'error' : ''}`;
+
+  return (
+    <div className="input-container">
+      <label>{placeholder}</label>
+      <input
+        className={inputClassName}
+        type={type}
+        placeholder={placeholder}
+        value={value}
+        onChange={change}
+        onFocus={handleFocus}
+        onBlur={handleBlur}
+      />
+      {error && <span className="error-text">{error}</span>}
+    </div>
+  );
+};
+
+export default ErrorInput;
