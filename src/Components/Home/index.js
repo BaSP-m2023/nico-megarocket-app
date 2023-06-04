4fae8b1e7f938155b2ca83cc26c83329e1834baa
FrancoLelli <francogastonlelli@gmail.com>
2023-06-04 14:44:46 -0300

    MR-88: Create folder Button in Shared

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,9 +1,11 @@
 import styles from './home.module.css';
+import { Button } from '../Shared';
 
 function Home() {
   return (
     <section className={styles.container}>
       <h2>Home</h2>
+      <Button clickAction={() => console.log('hola')} text="Add" />
     </section>
   );
 }
