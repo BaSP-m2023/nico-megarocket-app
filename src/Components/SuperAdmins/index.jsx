e9c0afbad54b86a3da3f883f3cc74854dd73992f
unknown <ivanjukonis@hotmail.com>
2023-06-06 21:49:55 -0300

    MR-106: Code consistency and order.

--- src/Components/SuperAdmins/index.jsx
+++ src/Components/SuperAdmins/index.jsx
@@ -5,11 +5,12 @@ import { ToastError, AddButton, TableComponent } from '../Shared';
 
 const SuperAdmins = () => {
   const [superAdmins, setSuperAdmins] = useState([]);
-  /*const [superAdminForm, setSuperAdminForm] = useState({
-    email: '',
-    password: ''
-  });*/
   const [toastErroOpen, setToastErroOpen] = useState(false);
+  const history = useHistory();
+
+  useEffect(() => {
+    getSuperAdmins();
+  }, []);
 
   const getSuperAdmins = async () => {
     try {
@@ -21,11 +22,6 @@ const SuperAdmins = () => {
       console.error(error);
     }
   };
-
-  useEffect(() => {
-    getSuperAdmins();
-  }, []);
-
   const deleteItem = async (id) => {
     try {
       await fetch(`${process.env.REACT_APP_API_URL}/api/super-admin/${id}`, {
@@ -40,69 +36,10 @@ const SuperAdmins = () => {
     }
   };
 
-  /*const addItem = async ({ email, password }) => {
-    try {
-      const newSuperAdmin = {
-        email,
-        password
-      };
-      const response = await fetch(`${process.env.REACT_APP_API_URL}/api/super-admin`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(newSuperAdmin)
-      });
-      if (response.ok) {
-        setSuperAdmins([...superAdmins, newSuperAdmin]);
-      } else {
-        console.error('Failed to add super admin');
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  };*/
-
-  /*const updateItem = async (updatedItem) => {
-    try {
-      // eslint-disable-next-line no-unused-vars
-      const { _id, __v, ...updatedData } = updatedItem;
-      console.log(JSON.stringify(updatedData));
-      const response = await fetch(`${process.env.REACT_APP_API_URL}/api/super-admin/${_id}`, {
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(updatedData)
-      });
-      if (response.ok) {
-        const updatedList = superAdmins.map((item) => {
-          if (item._id === updatedItem._id) {
-            return updatedItem;
-          }
-          return item;
-        });
-        setSuperAdmins(updatedList);
-        setSuccessMessage('The Super Admin has been updated successfully.');
-        setModalSuccessOpen(true);
-      } else {
-        console.error('Failed to update item');
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  };*/
-
-  /*const handleDeleteButtonClick = (id) => {
-    deleteItem(id);
-  };*/
-
-  const history = useHistory();
-
+  
   const handleClick = (item) => {
     history.push(`super-admins/form/${item._id}`, { params: { ...item, mode: 'edit' } });
   };
-
   const createMode = () => {
     history.push('/super-admins/form/', { params: { mode: 'create' } });
   };
