2e462adebe50b3ab9be22831cb13a00109c254e8
Mateo Carciente <mateocarciente1@gmail.com>
2023-06-11 11:44:54 -0300

    MR-155: fix hook errors

--- src/Components/Admins/index.js
+++ src/Components/Admins/index.js
@@ -7,9 +7,9 @@ import { getAllAdmins, adminDelete } from '../../redux/admins/thunks';
 function Admins() {
   const admins = useSelector((state) => state.admins.list);
   const isPending = useSelector((state) => state.admins.pending);
+  const isError = useSelector((state) => state.admins.error);
   const dispatch = useDispatch();
-  const [toastErroOpen, setToastErroOpen] = useState(false);
-  const [toastMessage, setToastMessage] = useState('Error in database');
+  const [toastErroOpen, setToastErroOpen] = useState(isError);
 
   const history = useHistory();
   const columnTitleArray = ['Admin', 'DNI', 'Phone', 'E-Mail', 'City'];
@@ -19,32 +19,37 @@ function Admins() {
     history.push(`/admins/form/${item._id}`, { params: { item, mode: 'edit' } });
   };
 
+  const createMode = () => {
+    history.push('/admins/form', { params: { mode: 'create' } });
+  };
+
   useEffect(() => {
     getAllAdmins(dispatch);
   }, []);
 
   useEffect(() => {
-    setToastMessage(useState('Error in database'));
-  }, []);
-
-  const createMode = () => {
-    history.push('/admins/form', { params: { mode: 'create' } });
-  };
+    setToastErroOpen(!isError);
+  }, [isError]);
 
   return (
     <section>
       <div>
         <AddButton entity="Admin" createMode={createMode} />
       </div>
-      {isPending && <Loader />} : (
-      <TableComponent
-        columnTitleArray={columnTitleArray}
-        data={admins}
-        handleClick={handleEditClick}
-        deleteButton={adminDelete}
-        columns={columns}
-      />
-      ){toastErroOpen && <ToastError setToastErroOpen={setToastErroOpen} message={toastMessage} />}
+      {isPending ? (
+        <Loader />
+      ) : (
+        <TableComponent
+          columnTitleArray={columnTitleArray}
+          data={admins}
+          handleClick={handleEditClick}
+          deleteButton={adminDelete}
+          columns={columns}
+        />
+      )}
+      {toastErroOpen && (
+        <ToastError setToastErroOpen={setToastErroOpen} message="Error in database" />
+      )}
     </section>
   );
 }
