1625b9afb4ee544154ef07b5093c920363cb1a00
unknown <ivanjukonis@hotmail.com>
2023-06-12 14:39:42 -0300

    MR-161: Redux implementation (actions and constants) into GET and UPDATE methods.

--- src/redux/activities/actions.js
+++ src/redux/activities/actions.js
@@ -4,7 +4,13 @@ import {
   GET_ACTIVITIES_ERROR,
   DELETE_ACTIVITIES_PENDING,
   DELETE_ACTIVITIES_SUCCESS,
-  DELETE_ACTIVITIES_ERROR
+  DELETE_ACTIVITIES_ERROR,
+  ADD_ACTIVITIES_PENDING,
+  ADD_ACTIVITIES_SUCCESS,
+  ADD_ACTIVITIES_ERROR,
+  UPDATE_ACTIVITIES_PENDING,
+  UPDATE_ACTIVITIES_SUCCESS,
+  UPDATE_ACTIVITIES_ERROR
 } from './constants';
 
 export const getActivitiesPending = (pending) => {
@@ -45,6 +51,48 @@ export const deleteActivitiesSuccess = (activitiesId) => {
 export const deleteActivitiesError = (error) => {
   return {
     type: DELETE_ACTIVITIES_ERROR,
+    payload: error
+  };
+};
+
+export const addActivitiesPending = (pending) => {
+  return {
+    type: ADD_ACTIVITIES_PENDING,
+    payload: pending
+  };
+};
+
+export const addActivitiesSuccess = (list) => {
+  return {
+    type: ADD_ACTIVITIES_SUCCESS,
+    payload: list
+  };
+};
+
+export const addActivitiesError = (error) => {
+  return {
+    type: ADD_ACTIVITIES_ERROR,
+    payload: error
+  };
+};
+
+export const updateActivitiesPending = (pending) => {
+  return {
+    type: UPDATE_ACTIVITIES_PENDING,
+    payload: pending
+  };
+};
+
+export const updateActivitiesSuccess = (list) => {
+  return {
+    type: UPDATE_ACTIVITIES_SUCCESS,
+    payload: list
+  };
+};
+
+export const updateActivitiesError = (error) => {
+  return {
+    type: UPDATE_ACTIVITIES_ERROR,
     payload: error
   };
 };
