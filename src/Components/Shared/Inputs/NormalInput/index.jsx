eea8f9be5d88b247d10319e0d0541841f51d2183
FrancoLelli <francogastonlelli@gmail.com>
2023-06-17 12:42:25 -0300

    MR-207: Modify input

--- src/Components/Shared/Inputs/NormalInput/index.jsx
+++ src/Components/Shared/Inputs/NormalInput/index.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import styles from './disable-input.module.css';
 
-const Inputs = ({ text, type, isDisabled, change, nameInput, nameTitle }) => {
+const Inputs = ({ register, error, type, isDisabled, nameInput, nameTitle }) => {
   return (
     <div className={styles.nameLabel}>
       <label>{nameTitle}</label>
       <input
+        {...register(nameInput, { required: { value: true, message: 'This field is required' } })}
         className={
           type === 'disable' ? `${styles.disableInput} ${styles.normalInput}` : styles.normalInput
         }
         type={type}
         disabled={isDisabled}
-        value={text}
         name={nameInput}
-        onChange={change}
       />
+      {error && <p>{error}</p>}
     </div>
   );
 };
