f3bfea9e0cd7c44e9f07f5cb2767548f8a9832a4
YhanahiRosenthal <yhanahi16@gmail.com>
2023-06-16 23:00:02 -0300

    MR-189: Navbar: styles modified and change routes

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -4,12 +4,13 @@ import { NavLink, useLocation } from 'react-router-dom/cjs/react-router-dom.min'
 
 const member = `${process.env.PUBLIC_URL}/assets/images/member.png`;
 const home = `${process.env.PUBLIC_URL}/assets/images/home.png`;
-const admin = `${process.env.PUBLIC_URL}/assets/images/admin.png`;
+const hombeB = `${process.env.PUBLIC_URL}/assets/images/HomeB.png`;
 const trainer = `${process.env.PUBLIC_URL}/assets/images/trainer.png`;
 const classs = `${process.env.PUBLIC_URL}/assets/images/class.png`;
 const suscription = `${process.env.PUBLIC_URL}/assets/images/suscription.png`;
 const members = `${process.env.PUBLIC_URL}/assets/images/muscle.png`;
 const activities = `${process.env.PUBLIC_URL}/assets/images/runner.png`;
+const login = `${process.env.PUBLIC_URL}/assets/images/login.png`;
 
 export const Navbar = () => {
   const house = useLocation().pathname;
@@ -17,7 +18,29 @@ export const Navbar = () => {
   return (
     <nav className={styles.navbar}>
       <ul className={styles.rutes}>
-        {house === '/home' && (
+        {house.includes('/home') && (
+          <>
+            <li className={styles.homeButtons}>
+              <NavLink to="/home" className={styles.navbarLinkButtons}>
+                <img src={hombeB} alt="icon home" />
+                Home
+              </NavLink>
+            </li>
+            <li className={styles.homeButtons}>
+              <NavLink to="/admin" className={styles.navbarLinkButtons}>
+                <img src={login} alt="icon login" />
+                Admins
+              </NavLink>
+            </li>
+            <li className={styles.homeButtons}>
+              <NavLink to="/member" className={styles.navbarLinkButtons}>
+                <img src={login} alt="icon login" />
+                Members
+              </NavLink>
+            </li>
+          </>
+        )}
+        {house.startsWith('/admin') && (
           <>
             <li className={styles.anchor}>
               <NavLink to="/home" className={styles.navbarLink} activeClassName={styles.active}>
@@ -25,56 +48,108 @@ export const Navbar = () => {
                 Home
               </NavLink>
             </li>
-            <button>admins</button>
-            <button>members</button>
+            <li>
+              <NavLink
+                to="/admin/classes"
+                className={styles.navbarLink}
+                activeClassName={styles.active}
+              >
+                <img src={classs} alt="icon class" />
+                Classes
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/admin/trainers"
+                className={styles.navbarLink}
+                activeClassName={styles.active}
+              >
+                <img src={trainer} alt="icon trainer" />
+                Trainers
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/admin/activities"
+                className={styles.navbarLink}
+                activeClassName={styles.active}
+              >
+                <img src={activities} alt="icon activities" />
+                Activities
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/admin/subscriptions"
+                className={styles.navbarLink}
+                activeClassName={styles.active}
+              >
+                <img src={suscription} alt="icon suscription" />
+                Subscriptions
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/admin/members"
+                className={styles.navbarLink}
+                activeClassName={styles.active}
+              >
+                <img src={members} alt="icon member" />
+                Members
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/admin/profile"
+                className={styles.navbarLink}
+                activeClassName={styles.active}
+              >
+                <img src={member} alt="icon member" />
+                Profile
+              </NavLink>
+            </li>
           </>
         )}
-
-        <li>
-          <NavLink to="/admins" className={styles.navbarLink} activeClassName={styles.active}>
-            <img src={admin} alt="icon admin" className={styles.members} />
-            Admins
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/members" className={styles.navbarLink} activeClassName={styles.active}>
-            <img src={members} alt="icon member" />
-            Members
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/activities" className={styles.navbarLink} activeClassName={styles.active}>
-            <img src={activities} alt="icon activities" />
-            Activities
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/classes" className={styles.navbarLink} activeClassName={styles.active}>
-            <img src={classs} alt="icon class" />
-            Classes
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to="/subscriptions"
-            className={styles.navbarLink}
-            activeClassName={styles.active}
-          >
-            <img src={suscription} alt="icon suscription" />
-            Subscriptions
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/super-admins" className={styles.navbarLink} activeClassName={styles.active}>
-            <img src={member} alt="icon superadmin" /> SuperAdmins
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/trainers" className={styles.navbarLink} activeClassName={styles.active}>
-            <img src={trainer} alt="icon trainer" />
-            Trainers
-          </NavLink>
-        </li>
+        {house.startsWith('/member') && (
+          <>
+            <li className={styles.anchor}>
+              <NavLink to="/home" className={styles.navbarLink} activeClassName={styles.active}>
+                <img src={home} alt="icon home" />
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/member/classes"
+                className={styles.navbarLink}
+                activeClassName={styles.active}
+              >
+                <img src={classs} alt="icon class" />
+                Classes
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/member/activities"
+                className={styles.navbarLink}
+                activeClassName={styles.active}
+              >
+                <img src={activities} alt="icon activities" />
+                Activities
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/member/profile"
+                className={styles.navbarLink}
+                activeClassName={styles.active}
+              >
+                <img src={member} alt="icon member" />
+                Profile
+              </NavLink>
+            </li>
+          </>
+        )}
       </ul>
     </nav>
   );
