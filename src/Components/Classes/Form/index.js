9fd99d8c9f29b600f6e6918b0bb50040810787f1
Mauro Jimenez <maurojim123@gmail.com>
2023-06-18 12:51:32 -0300

    MR-178: react hook form on classes and option input styles

--- src/Components/Classes/Form/index.js
+++ src/Components/Classes/Form/index.js
@@ -9,10 +9,10 @@ import { getAllActivities } from './../../../redux/activities/thunks';
 import { useForm } from 'react-hook-form';
 
 const FormClasses = () => {
-  const [modalUpdateConfirmOpen, setModalUpdateConfirmOpen] = useState(false);
+  const [modalConfirmOpen, setModalConfirmOpen] = useState(false);
   const [modalSuccessOpen, setModalSuccessOpen] = useState(false);
-  const [successMessage, setSuccessMessage] = useState('');
-  const [inputForm, setInputForm] = useState('');
+  const [updClass, setUpdClass] = useState({});
+  const [trainerChange, setTrainerChange] = useState('');
   const { id } = useParams();
   const locationObject = useLocation();
   const updateData = locationObject.state.params;
@@ -21,100 +21,67 @@ const FormClasses = () => {
   const activities = useSelector((state) => state.activities.list);
   const dispatch = useDispatch();
   const history = useHistory();
-
   const updateItem = classes.find((item) => item._id === id);
 
-  const { register, handleSubmit } = useForm();
+  const classesData = {
+    activity: updateItem?.activity ? updateItem.activity._id : '',
+    day: updateItem?.day,
+    hour: updateItem?.hour,
+    slots: updateItem?.slots,
+    trainer: updateItem?.trainer ? updateItem.trainer.map((item) => item._id) : []
+  };
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors }
+  } = useForm({
+    mode: 'onBlur',
+    defaultValues: {
+      ...classesData
+    }
+  });
 
   useEffect(() => {
     getClasses(dispatch);
     getTrainers(dispatch);
     getAllActivities(dispatch);
+    setTrainerChange(classesData.trainer[0]);
   }, []);
-
-  useEffect(() => {
-    {
-      updateData.mode === 'edit'
-        ? setInputForm({
-            hour: updateItem?.hour,
-            day: updateItem?.day,
-            trainer: updateItem?.trainer ? updateItem.trainer.map((item) => item._id) : '',
-            activity: updateItem?.activity ? updateItem.activity._id : '',
-            slots: updateItem?.slots
-          })
-        : setInputForm({ hour: '', day: '', trainer: '', activity: '', slots: '' });
-    }
-  }, [classes && classes.length === 0]);
 
   const classBody = {
     method: updateData.mode === 'edit' ? 'PUT' : 'POST',
     headers: {
       'Content-Type': 'application/json'
     },
-    body: JSON.stringify(inputForm)
+    body: JSON.stringify(updClass)
   };
 
-  const handleUpdateButtonClick = () => {
-    setModalUpdateConfirmOpen(true);
-  };
-
-  const handleModalConfirmation = () => {
-    updateClass(id, classBody, dispatch);
-    setModalUpdateConfirmOpen(false);
-    setSuccessMessage('The class has been updated successfully.');
-    setModalSuccessOpen(true);
-    setTimeout(() => {
-      history.push('/admin/classes');
-      setModalSuccessOpen(false);
-    }, 2000);
-  };
-
-  const onChangeHour = (e) => {
-    setInputForm({
-      ...inputForm,
-      hour: e.target.value
-    });
-  };
-
-  const onChangeDay = (e) => {
-    setInputForm({
-      ...inputForm,
-      day: e.target.value
-    });
-  };
-
-  const onChangeTrainer = (e) => {
-    setInputForm({
-      ...inputForm,
-      trainer: [e.target.value]
-    });
-  };
-
-  const onChangeActivity = (e) => {
-    setInputForm({
-      ...inputForm,
-      activity: e.target.value
-    });
-  };
-
-  const onChangeSlots = (e) => {
-    setInputForm({
-      ...inputForm,
-      slots: e.target.value
-    });
-  };
-
-  const formSubmit = (e) => {
-    e.preventDefault();
-
-    if (updateData.mode === 'edit') {
-      handleUpdateButtonClick();
+  const openModal = async (data) => {
+    if (typeof data.trainer === 'object') {
+      setTrainerChange(data.trainer[0]);
     } else {
-      createClass(classBody, dispatch);
-      setSuccessMessage('The class has been created successfully.');
-      setModalSuccessOpen(true);
+      setTrainerChange(data.trainer);
+    }
+    const classArrayTrainer = { ...data, trainer: [trainerChange] };
+    setModalConfirmOpen(true);
+    setUpdClass(classArrayTrainer);
+  };
+
+  const formSubmit = async () => {
+    if (updateData.mode === 'edit') {
+      setModalConfirmOpen(false);
+      await updateClass(id, classBody, dispatch);
+      await setModalSuccessOpen(true);
       setTimeout(() => {
-        history.push('/classes');
+        history.push('/admin/classes');
+        setModalSuccessOpen(false);
+      }, 2000);
+    } else {
+      await createClass(classBody, dispatch);
+      await setModalSuccessOpen(true);
+      setTimeout(() => {
+        history.goBack();
         setModalSuccessOpen(false);
       }, 2000);
     }
@@ -122,7 +89,7 @@ const FormClasses = () => {
 
   return (
     <div className={formStyles.container}>
-      <form className={formStyles.form} onSubmit={handleSubmit}>
+      <form className={formStyles.form} onSubmit={handleSubmit(openModal)}>
         <div className={formStyles.container}>
           <h2 className={formStyles.formTitle}>
             {updateData.mode === 'edit' ? 'Update' : 'Create'} Class
@@ -131,68 +98,83 @@ const FormClasses = () => {
             <Inputs
               type={'text'}
               isDisabled={false}
-              change={onChangeHour}
               nameInput={'hour'}
               nameTitle="Hour"
               register={register}
+              error={errors.hour?.message}
             />
 
             <Inputs
               type={'text'}
               isDisabled={false}
-              change={onChangeDay}
               nameInput={'day'}
               nameTitle="Day"
               register={register}
+              error={errors.day?.message}
             />
 
             <OptionInput
               data={trainers}
               dataLabel="Trainers"
-              onChangeOption={onChangeTrainer}
-              setValue={inputForm.trainer}
+              setValue={setValue}
+              aValue={classesData.trainer}
               name="trainer"
+              register={register}
+              error={errors.trainer?.message}
             />
 
             <OptionInput
               data={activities}
               dataLabel="Activities"
-              onChangeOption={onChangeActivity}
-              setValue={inputForm.activity}
+              setValue={setValue}
+              aValue={classesData.activity}
               name="activity"
+              register={register}
+              error={errors.activity?.message}
             />
 
             <Inputs
               type={'text'}
               isDisabled={false}
-              change={onChangeSlots}
               nameInput={'slots'}
               nameTitle="Slots"
               register={register}
+              error={errors.slots?.message}
             />
           </div>
           <div className={formStyles.buttons}>
             <span className={formStyles.cancelButton}>
-              <Button clickAction={() => history.goBack()} text="Cancel" />
+              <Button clickAction={() => history.push('/admin/classes')} text="Cancel" />
             </span>
             <Button
-              clickAction={formSubmit}
+              clickAction={() => {}}
               text={updateData.mode === 'edit' ? 'Update' : 'Create'}
             />
           </div>
         </div>
       </form>
-      {modalUpdateConfirmOpen && (
+      {modalConfirmOpen && (
         <ModalConfirm
-          method="Update"
-          onConfirm={handleModalConfirmation}
-          setModalConfirmOpen={setModalUpdateConfirmOpen}
-          message="Are you sure you want to update this?"
+          method={updateData.mode === 'edit' ? 'Edit' : 'Create'}
+          onConfirm={formSubmit}
+          setModalConfirmOpen={setModalConfirmOpen}
+          message={
+            updateData.mode === 'edit'
+              ? 'Are you sure you want to update this?'
+              : 'Are you sure you want to create this?'
+          }
         />
       )}
       <div>
         {modalSuccessOpen && (
-          <ModalSuccess setModalSuccessOpen={setModalSuccessOpen} message={successMessage} />
+          <ModalSuccess
+            setModalSuccessOpen={setModalSuccessOpen}
+            message={
+              updateData.mode === 'edit'
+                ? 'The class has been updated successfully'
+                : 'The class has been created successfully'
+            }
+          />
         )}
       </div>
     </div>
