57e7bf3d575de34046f1719f83bbd0df00106285
Mauro Jimenez <maurojim123@gmail.com>
2023-06-17 21:56:58 -0300

    MR-182: Reset buttons instalation and some design changes

--- src/Components/SuperAdmins/Form/index.jsx
+++ src/Components/SuperAdmins/Form/index.jsx
@@ -1,6 +1,6 @@
+import styles from './form.module.css';
 import React, { useState, useEffect } from 'react';
 import { ModalConfirm, Inputs, Button, ModalSuccess, Loader, ToastError } from '../../Shared';
-import styles from './form.module.css';
 import { useHistory, useParams, useLocation } from 'react-router-dom/cjs/react-router-dom.min';
 import { addSuperAdmin, updateSuperAdmin } from '../../../redux/superAdmins/thunks';
 import { useDispatch, useSelector } from 'react-redux';
@@ -9,14 +9,24 @@ import Joi from 'joi';
 import { joiResolver } from '@hookform/resolvers/joi';
 
 const schema = Joi.object({
-  email: Joi.string().email({ tlds: { allow: false } }),
+  email: Joi.string()
+    .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } })
+    .messages({
+      'string.base': 'The email must be a text string',
+      'string.empty': 'The email is a required field',
+      'string.email': 'The email must be a valid email address',
+      'string.minDomainSegments': 'The email must have at least 2 domain segments',
+      'string.tlds.allow': 'The email must have a valid top-level domain (.com or .net)'
+    }),
   password: Joi.string()
     .min(8)
     .pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/)
     .required()
     .messages({
       'string.pattern.base':
-        'The password must contain at least one lowercase letter, one uppercase letter, and one digit'
+        'The password must contain at least one lowercase letter, one uppercase letter, and one digit',
+      'string.min': 'The password must be at least 8 characters long',
+      'string.empty': 'The password field is required'
     })
 });
 
@@ -25,6 +35,7 @@ const Form = () => {
   const [modalConfirmOpen, setModalConfirmOpen] = useState(false);
   const [toastErrorOpen, setToastErrorOpen] = useState(false);
   const [editMode, setEditMode] = useState(false);
+  const [supAdm, setSupAdm] = useState({});
 
   const location = useLocation();
   const history = useHistory();
@@ -34,15 +45,16 @@ const Form = () => {
   const dispatch = useDispatch();
   const isLoading = useSelector((state) => state.superAdmin.loading);
   const anError = useSelector((state) => state.superAdmin.error);
-  const [supAdm, setSupAdm] = useState({});
 
   const editSupAdm = {
     email: params.email,
     password: params.password
   };
+
   const {
     handleSubmit,
     register,
+    reset,
     formState: { errors }
   } = useForm({
     resolver: joiResolver(schema),
@@ -94,31 +106,34 @@ const Form = () => {
 
   return (
     <form className={styles.formSuperAdmin} onSubmit={handleSubmit(openModal)}>
-      <div className={styles.containerForm}>
-        <Inputs
-          type="email"
-          nameInput={'email'}
-          nameTitle={'Email'}
-          register={register}
-          error={errors.email?.message}
-        />
-        <Inputs
-          type="password"
-          nameInput={'password'}
-          nameTitle={'Password'}
-          register={register}
-          error={errors.password?.message}
-        />
-      </div>
-      <div className={styles.sub_buttons}>
-        <Button clickAction={() => {}} text="Submit" />
-        <Button
-          clickAction={(e) => {
-            e.preventDefault();
-            history.goBack();
-          }}
-          text="Cancel"
-        />
+      <div className={styles.wholeContainer}>
+        <div className={styles.containerForm}>
+          <Inputs
+            type="email"
+            nameInput={'email'}
+            nameTitle={'Email'}
+            register={register}
+            error={errors.email?.message}
+          />
+          <Inputs
+            type="password"
+            nameInput={'password'}
+            nameTitle={'Password'}
+            register={register}
+            error={errors.password?.message}
+          />
+        </div>
+        <div className={styles.sub_buttons}>
+          <Button clickAction={() => {}} text="Submit" />
+          <Button clickAction={() => reset()} text="Reset" />
+          <Button
+            clickAction={(e) => {
+              e.preventDefault();
+              history.goBack();
+            }}
+            text="Cancel"
+          />
+        </div>
       </div>
 
       {modalConfirmOpen && (
