da99d5ac925b666a9f17950fffcce3dc90c7d726
Mateo Carciente <mateocarciente1@gmail.com>
2023-06-20 18:33:17 -0300

    MR-186: fix errors from module.css

--- src/Components/Users/Member/activities/Cards/index.jsx
+++ src/Components/Users/Member/activities/Cards/index.jsx
@@ -1,15 +1,31 @@
 import styles from './cards.module.css';
-import React from 'react';
+import React, { useState } from 'react';
 
 function Cards({ title, description, image }) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleButtonClick = () => {
+    setIsModalOpen(true);
+  };
   return (
     <div className={styles.container}>
       <div className={styles.card}>
         <img className={styles.cardImage} src={image}></img>
         <h2>{title}</h2>
         <p>{description}</p>
-        <a href="">Learn More</a>
+        <button onClick={handleButtonClick}>Learn More</button>
       </div>
+
+      {isModalOpen && (
+        <div className={styles.modal}>
+          <div className={styles.modalContent}>
+            <h3>{title}</h3>
+            <p>{description}</p>
+            {/* Otros elementos del modal */}
+            <button onClick={() => setIsModalOpen(false)}>Close</button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
