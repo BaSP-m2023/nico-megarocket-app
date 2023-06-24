f1c84c97866a5f115ece390fa255b8f05f3bc5d0
FrancoLelli <francogastonlelli@gmail.com>
2023-06-24 10:56:49 -0300

    MR-246: Add routes and functions from react router dom

---
+++ src/routes/auth.js
@@ -0,0 +1,18 @@
+/* import { Switch, Route, Redirect, useRouteMatch } from 'react-router-dom';
+ */
+/* const routes = [
+  {
+    name: 'Login',
+    path: '/auth/login'
+  },
+  {
+    name: 'Sign Up',
+    path: '/auth/sign-up'
+  }
+];
+ */
+const AuthRoute = () => {
+  return <div>AuthRoute</div>;
+};
+
+export default AuthRoute;
