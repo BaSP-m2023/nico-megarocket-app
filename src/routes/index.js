64d32b05af911c2105562ec881ed348d0cdd9afc
FrancoLelli <francogastonlelli@gmail.com>
2023-06-25 17:17:03 -0300

    MR-254: Create Routes }component

---
+++ src/routes/index.js
@@ -0,0 +1,15 @@
+/* import { lazy, Suspense } from 'react';
+import { Switch, Route, BrowserRouter, Redirect } from 'react-router-dom';
+import PrivateRoute from './privateRoute'; */
+
+/* const SuperAdminRoutes = '';
+const AdminRoutes = '';
+const TrainerRoutes = '';
+const MemberRoute = '';
+const AuthRoute = ''; */
+
+const Routes = () => {
+  return <p>Route</p>;
+};
+
+export default Routes;
