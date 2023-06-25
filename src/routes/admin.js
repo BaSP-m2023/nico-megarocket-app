658d05492ed2e1147093f09db8fb70287c61b8df
FrancoLelli <francogastonlelli@gmail.com>
2023-06-25 18:18:59 -0300

    MR-254: Add Classes and profile component

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -5,8 +5,15 @@ import Members from 'Components/Members';
 import MembersForm from 'Components/Members/Form/MembersForm';
 import Trainers from 'Components/Trainers';
 import FormTrainer from 'Components/Trainers/FormTrainers';
+import Classes from 'Components/Classes';
+import FormClasses from 'Components/Classes/Form';
+import AdminProfile from 'Components/Users/Admin/profile';
 
 const routes = [
+  {
+    name: 'Classes',
+    path: '/admin/classes'
+  },
   {
     name: 'Members',
     path: '/admin/members'
@@ -14,6 +21,10 @@ const routes = [
   {
     name: 'Trainers',
     path: '/admin/trainers'
+  },
+  {
+    name: 'Profile',
+    path: '/admin/profile'
   }
 ];
 
@@ -22,10 +33,13 @@ const AdminRoutes = () => {
   return (
     <Layout routes={routes}>
       <Switch>
+        <Route exact path={`${url}/classes`} component={Classes} />
+        <Route path={`${url}/classes/form/:id?`} component={FormClasses} />
         <Route exact path={`${url}/trainers`} component={Trainers} />
         <Route path={`${url}/trainers/form/:id?`} component={FormTrainer} />
         <Route exact path={`${url}/members`} component={Members} />
         <Route path={`${url}/members/form/:id?`} component={MembersForm} />
+        <Route exact path={`${url}/admin/profile`} component={AdminProfile} />
         <Redirect path={`${url}/login`} />
       </Switch>
     </Layout>
