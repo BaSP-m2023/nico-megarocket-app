8393f93a986997597ba87171e46937794e530592
FrancoLelli <francogastonlelli@gmail.com>
2023-06-28 16:22:42 -0300

    MR-282: Update DivContainer

--- src/Components/Shared/Containers/index.js
+++ src/Components/Shared/Containers/index.js
@@ -11,28 +11,31 @@ function DivContainer({ item, testId }) {
   const [toggle, setToggle] = useState(true);
   const [modalConfirm, setModalConfirm] = useState(false);
   const [userCurrent, setUserCurrent] = useState('');
+  const [memberID, setMemberId] = useState('');
   const dispatch = useDispatch();
   const subscriptions = useSelector((state) => state.subscription.data);
   const members = useSelector((state) => state.members.list);
-
   const member = members.find((oneMember) => oneMember.email === userCurrent);
+
+  console.log(subscriptions);
 
   const currentUser = async () => {
     try {
       const emailCurrentUser = await getFirebaseUidFromToken();
       setUserCurrent(emailCurrentUser);
-      existMemberInClass();
+      setMemberId(member._id);
+      existMemberInClass(memberID);
     } catch (error) {
       return error;
     }
   };
 
-  const existMemberInClass = () => {
+  const existMemberInClass = (memberID) => {
     const { _id: classId } = item;
     const classInSubscription = subscriptions.find((sub) => sub.classId._id === classId);
     if (classInSubscription) {
       classInSubscription.members.map((memberInSub) => {
-        if (memberInSub._id === member._id) {
+        if (memberInSub._id === memberID || memberInSub === memberID) {
           setToggle(false);
         }
       });
@@ -43,32 +46,42 @@ function DivContainer({ item, testId }) {
     setToggle(!toggle);
     const audioLabel = document.createElement('audio');
     const idToUpdate = item._id;
-    const susbscription = subscriptions.filter((subs) => subs.classId._id === idToUpdate);
+    const susbscription = await subscriptions.filter((subs) => {
+      return subs.classId._id === idToUpdate;
+    });
     const subscriptionID = susbscription[0]._id;
     if (toggle) {
       audioLabel.setAttribute('src', `${process.env.PUBLIC_URL}/assets/sounds/yeahBuddy.mp3`);
 
       const slotLess = { slots: item.slots - 1 };
-      await updateClass(idToUpdate, slotLess, dispatch);
+      await dispatch(updateClass(idToUpdate, slotLess));
 
-      const memberIds = susbscription[0].members.map((member) => member._id);
-      memberIds.push(member._id);
-      await updateSubscriptions(dispatch, subscriptionID, {
-        members: memberIds
+      const memberIds = susbscription[0].members.map((member) => {
+        return member._id;
       });
+      memberIds.push(memberID);
+      await dispatch(
+        updateSubscriptions(subscriptionID, {
+          members: memberIds
+        })
+      );
     } else {
       audioLabel.setAttribute('src', `${process.env.PUBLIC_URL}/assets/sounds/lightWeight.mp3`);
 
-      const slotAdd = { slots: item.slots + 1 };
-      await updateClass(idToUpdate, slotAdd, dispatch);
+      const slotAdd = { slots: item.slots };
+      await dispatch(updateClass(idToUpdate, slotAdd));
 
       const memberWithoutCurrent = susbscription[0].members.filter(
-        (memberInArray) => memberInArray._id !== member._id
+        (memberInArray) => memberInArray._id !== memberID
       );
-      const memberIds = memberWithoutCurrent.map((member) => member._id);
-      await updateSubscriptions(dispatch, subscriptionID, {
-        members: memberIds
+      const memberIds = memberWithoutCurrent.map((member) => {
+        return member._id;
       });
+      await dispatch(
+        updateSubscriptions(subscriptionID, {
+          members: memberIds
+        })
+      );
     }
     audioLabel.play();
   };
