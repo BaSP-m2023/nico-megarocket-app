858c489701859a404b8a5030a678defac90594b6
FrancoLelli <francogastonlelli@gmail.com>
2023-06-28 16:39:54 -0300

    MR-283: Update thunks in class

--- src/redux/classes/thunks.js
+++ src/redux/classes/thunks.js
@@ -13,20 +13,22 @@ import {
   editClassError
 } from './actions';
 
-export const getClasses = async (dispatch) => {
-  try {
-    dispatch(getClassPending(true));
-    const response = await fetch(`${process.env.REACT_APP_API_URL}/api/class`);
-    const data = await response.json();
-    dispatch(getClassPending(false));
-    if (response.status === 200) {
-      dispatch(getClassSuccess(data.data));
-    } else {
-      dispatch(getClassError(data.message));
+export const getClasses = () => {
+  return async (dispatch) => {
+    try {
+      dispatch(getClassPending(true));
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/api/class`);
+      const data = await response.json();
+      dispatch(getClassPending(false));
+      if (response.status === 200) {
+        dispatch(getClassSuccess(data.data));
+      } else {
+        dispatch(getClassError(data.message));
+      }
+    } catch (error) {
+      dispatch(getClassPending(false));
     }
-  } catch (error) {
-    dispatch(getClassPending(false));
-  }
+  };
 };
 
 export const deleteClass = (id) => {
@@ -52,40 +54,44 @@ export const deleteClass = (id) => {
   };
 };
 
-export const createClass = async (body, dispatch) => {
-  try {
-    dispatch(addClassPending(true));
-    const response = await fetch(`${process.env.REACT_APP_API_URL}/api/class`, body);
-    const data = await response.json();
-    dispatch(addClassPending(false));
-    if (response.status !== 200) {
-      dispatch(addClassError(data.error));
-    } else {
-      dispatch(addClassSuccess(body));
+export const createClass = (body) => {
+  return async (dispatch) => {
+    try {
+      dispatch(addClassPending(true));
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/api/class`, body);
+      const data = await response.json();
+      dispatch(addClassPending(false));
+      if (response.status !== 200) {
+        dispatch(addClassError(data.error));
+      } else {
+        dispatch(addClassSuccess(body));
+      }
+    } catch (error) {
+      dispatch(addClassPending(false));
     }
-  } catch (error) {
-    dispatch(addClassPending(false));
-  }
+  };
 };
 
-export const updateClass = async (id, body, dispatch) => {
-  try {
-    dispatch(editClassPending(true));
-    const response = await fetch(`${process.env.REACT_APP_API_URL}/api/class/${id}`, {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(body)
-    });
-    const data = await response.json();
-    dispatch(editClassPending(false));
-    if (response.status !== 200) {
-      return dispatch(editClassError(data.message));
-    } else {
-      return dispatch(editClassSuccess(body));
+export const updateClass = (id, body) => {
+  return async (dispatch) => {
+    try {
+      dispatch(editClassPending(true));
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/api/class/${id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(body)
+      });
+      const data = await response.json();
+      dispatch(editClassPending(false));
+      if (response.status !== 200) {
+        return dispatch(editClassError(data.message));
+      } else {
+        return dispatch(editClassSuccess(body));
+      }
+    } catch (error) {
+      return dispatch(editClassPending(false));
     }
-  } catch (error) {
-    return dispatch(editClassPending(false));
-  }
+  };
 };
