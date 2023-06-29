1e2084e50b9421071f781efc8c406fa3d7686c5b
Barbimillan <barbii.millan@gmail.com>
2023-06-29 12:04:08 -0300

    MR-257: add test login member

---
+++ test/pageobjects/member.page.js
@@ -0,0 +1,94 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+
+class MemberPage {
+  get navBtn() {
+    return $('nav').$$('[data-testid="home-btn"]');
+  }
+
+  get logoutBtn() {
+    return $('nav').$$('[data-testid="log-out-btn"]');
+  }
+
+  get classesContainer() {
+    return $('[data-testid="classes-container"]');
+  }
+
+  get fulboCard() {
+    return $('[data-testid="fulbo-card"]');
+  }
+
+  get fulboCardBtn() {
+    return $('[data-testid="fulbo-card"] button');
+  }
+
+  get boxingCard() {
+    return $('[data-testid="Boxing-card"]');
+  }
+
+  get boxingCardBtn() {
+    return $('[data-testid="Boxing-card"] button');
+  }
+
+  get piscinaCard() {
+    return $('[data-testid="Piscina-card"]');
+  }
+
+  get piscinaCardBtn() {
+    return $('[data-testid="Piscina-card"] button');
+  }
+
+  get baseballCard() {
+    return $('[data-testid="Baseball-card"]');
+  }
+
+  get baseballCardBtn() {
+    return $('[data-testid="Baseball-card"] button');
+  }
+
+  get javascriptCard() {
+    return $('[data-testid="Javascript-card"]');
+  }
+
+  get javascriptCardBtn() {
+    return $('[data-testid="Javascript-card"] button');
+  }
+
+  get activityModal() {
+    return $('[data-testid="activity-modal"]');
+  }
+
+  get activityModalBtn() {
+    return $('[data-testid="activity-modal"] button');
+  }
+
+  get photo() {
+    return $('[data-testid="photo-container"]');
+  }
+
+  get infoProfile() {
+    return $('[data-testid="info-container"]');
+  }
+
+  async fulboCardBtnClick() {
+    return this.fulboCardBtn.click();
+  }
+
+  async boxingCardBtnClick() {
+    return this.boxingCardBtn.click();
+  }
+
+  async piscinaCardBtnClick() {
+    return this.piscinaCardBtn.click();
+  }
+
+  async baseballCardBtnClick() {
+    return this.baseballCardBtn.click();
+  }
+
+  async javascriptCardBtnClick() {
+    return this.javascriptCardBtn.click();
+  }
+}
+
+module.exports = new MemberPage();
--- test/specs/login.member.e2e.js
+++ test/specs/login.member.e2e.js
@@ -3,6 +3,8 @@
 
 const HomePage = require('../pageobjects/home.page.js');
 const Login = require('../pageobjects/login.page.js');
+const memberPage = require('../pageobjects/member.page.js');
+const MemberPage = require('../pageobjects/member.page.js');
 
 describe('login member', () => {
   beforeAll(() => {
@@ -12,6 +14,7 @@ describe('login member', () => {
 
   it('success process', async () => {
     await expect(HomePage.navBtn[1]).toBeDisplayed();
+    await expect(HomePage.navBtn[1]).toBeClickable();
     await HomePage.navBtn[1].click();
     const currentUrl = await browser.getUrl();
     expect(currentUrl).toEqual('https://nico-megarocket-app.vercel.app/auth/login');
@@ -25,6 +28,7 @@ describe('login member', () => {
   it('Login with all empty', async () => {
     await Login.loginForm('', '');
     await expect(Login.loginBtn).toBeDisplayed();
+    await expect(Login.loginBtn).toBeClickable();
     await Login.loginBtnClick();
     const errorEmailText = await Login.errorEmailInput.getText();
     expect(errorEmailText).toEqual('The email is a required field');
@@ -44,7 +48,41 @@ describe('login member', () => {
     await Login.loginForm('vigiyak708@edulena.com', '1234Gino');
     await expect(Login.loginBtn).toBeDisplayed();
     await Login.loginBtnClick();
-    // const currentUrl = await browser.getUrl();
-    // expect(currentUrl).toEqual('https://nico-megarocket-app.vercel.app/member');
+    const currentUrl = await browser.getUrl();
+    expect(currentUrl).toEqual('https://nico-megarocket-app.vercel.app/member');
+  });
+
+  it('See classes', async () => {
+    await expect(MemberPage.navBtn[0]).toBeDisplayed();
+    await expect(MemberPage.navBtn[0]).toBeClickable();
+    await MemberPage.navBtn[0].click();
+    const currentUrl = await browser.getUrl();
+    expect(currentUrl).toEqual('https://nico-megarocket-app.vercel.app/member/classes');
+    await expect(MemberPage.classesContainer).toBeDisplayed();
+  });
+
+  it('See activities', async () => {
+    await expect(MemberPage.navBtn[1]).toBeDisplayed();
+    await expect(MemberPage.navBtn[1]).toBeClickable();
+    await MemberPage.navBtn[1].click();
+    const currentUrl = await browser.getUrl();
+    expect(currentUrl).toEqual('https://nico-megarocket-app.vercel.app/member/activities');
+    await expect(MemberPage.fulboCard).toBeDisplayed();
+    await expect(MemberPage.boxingCard).toBeDisplayed();
+    await expect(MemberPage.piscinaCard).toBeDisplayed();
+    await expect(MemberPage.baseballCard).toBeDisplayed();
+    await expect(MemberPage.javascriptCard).toBeDisplayed();
+    await expect(MemberPage.fulboCardBtn).toBeDisplayed();
+    await expect(MemberPage.baseballCardBtn).toBeDisplayed();
+    await expect(MemberPage.piscinaCardBtn).toBeDisplayed();
+    await expect(MemberPage.baseballCardBtn).toBeDisplayed();
+    await expect(MemberPage.javascriptCardBtn).toBeDisplayed();
+    await expect(memberPage.fulboCardBtn).toBeClickable();
+    await expect(memberPage.boxingCardBtn).toBeClickable();
+    await expect(memberPage.piscinaCardBtn).toBeClickable();
+    await expect(memberPage.baseballCardBtn).toBeClickable();
+    await expect(memberPage.javascriptCardBtn).toBeClickable();
+    await MemberPage.fulboCardBtnClick();
+    await expect(MemberPage.activityModal).toBeDisplayed();
   });
 });
