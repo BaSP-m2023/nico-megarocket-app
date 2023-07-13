431b93e9dbc4385d2e9cb9d24f8c3df212458a35
Emanuel-Lamberti <elemanu9222@gmail.com>
2023-07-13 19:56:01 -0300

    MR-361: modify event in link

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,15 +1,24 @@
 import styles from './header.module.css';
+import { useHistory } from 'react-router-dom';
 import ProfileMenu from './ProfileMenu';
 
 function Header() {
   const token = sessionStorage.getItem('token');
   const logo = `${process.env.PUBLIC_URL}/assets/images/logo.png`;
-  const linkUrl = `http://localhost:3000/auth/home`;
+  const linkUrl = '/auth/home';
+
+  const history = useHistory();
+
+  const handleLinkClick = (event) => {
+    event.preventDefault();
+    history.push(linkUrl);
+  };
+
   return (
     <header>
       <div className={styles.container}>
         <div className={styles.containerLogo}>
-          <a href={linkUrl}>
+          <a href="#" onClick={handleLinkClick}>
             <div className={styles.imgLogo}>
               <img src={logo} alt="Rocket logo" />
             </div>
