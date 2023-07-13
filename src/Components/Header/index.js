d56a21f33511beac0fa199f4bb682f11f4ed53d6
Emanuel-Lamberti <elemanu9222@gmail.com>
2023-07-13 19:21:08 -0300

    MR-361: add link to Home in Header and modify loader border

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -4,14 +4,17 @@ import ProfileMenu from './ProfileMenu';
 function Header() {
   const token = sessionStorage.getItem('token');
   const logo = `${process.env.PUBLIC_URL}/assets/images/logo.png`;
+  const linkUrl = `http://localhost:3000/auth/home`;
   return (
     <header>
       <div className={styles.container}>
         <div className={styles.containerLogo}>
-          <div className={styles.imgLogo}>
-            <img src={logo} alt="Rocket logo" />
-          </div>
-          <div className={styles.brand}>Mega Rocket Gym</div>
+          <a href={linkUrl}>
+            <div className={styles.imgLogo}>
+              <img src={logo} alt="Rocket logo" />
+            </div>
+            <div className={styles.brand}>Mega Rocket Gym</div>
+          </a>
         </div>
         <div className={styles.profileMenu}>{token && <ProfileMenu />}</div>
       </div>
